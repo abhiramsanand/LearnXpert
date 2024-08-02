5d6fbae387e4ac4568f0301fe1ccc079655b3249
ashna-53 <ashnajamal104@gmail.com>
2024-08-02 11:59:29 +0530

    added whole report page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import AOS from 'aos';
 import AssessmentPage from "./pages/Trainee/AssessmentPage";
+import WholeReportPage from "./pages/Trainee/WholeReportPage";
 
 
 
@@ -20,7 +21,7 @@ function App() {
             <Route path="/Login" element={<Login />} />
             <Route path="/Signup" element={<Signup />} />
             <Route path="/AssessmentPage" element={<AssessmentPage />} />
-
+            <Route path="/WholeReportPage" element={<WholeReportPage />} />
           </Routes>
         </main>
       </div>
---
+++ src/pages/Trainee/WholeReportPage.tsx
@@ -0,0 +1,17 @@
+
+import ReportTable from '../../components/Trainee/WholeReport/ReportTable';
+
+
+const WholeReportPage: React.FC = () => {
+  
+
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>Reports</h1>
+           
+            <ReportTable/>
+        </div>
+    );
+};
+
+export default WholeReportPage;
