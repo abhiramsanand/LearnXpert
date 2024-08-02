086d5657e7a04c79f3fa020018cc78f535dbbcd0
athirark96 <athirabala10@gmail.com>
2024-08-02 09:59:09 +0530

    edited enquiry and course

---
+++ src/pages/Trainee/CoursePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CourseComponent from '../../components/Trainee/Course/Course'
+const CoursePage = () => {
+  return (
+    <div>
+      <CourseComponent/>
+    </div>
+  )
+}
+
+export default CoursePage
