f1df89cca189c339e6659b350f86086b5526275d
ashna-53 <ashnajamal104@gmail.com>
2024-08-05 10:58:14 +0530

    Added Back Button to WholeReport Page

--- src/pages/Trainee/WholeReportPage.tsx
+++ src/pages/Trainee/WholeReportPage.tsx
@@ -1,19 +1,22 @@
-
+import React from 'react';
 import ReportTable from '../../components/Trainee/WholeReport/ReportTable';
 import TraineeHeader from '../../shared components/TraineeHeader';
-
 import Footer from '../../shared components/Footer';
-const WholeReportPage: React.FC = () => {
-  
+import BackButton from '../../components/Trainee/WholeReport/BackButton';
 
-    return (
-        <div style={{ padding: '10px' }}>
-            <TraineeHeader title={'Whole Report'} />
-           
-            <ReportTable/>
-            <Footer/>
-        </div>
-    );
+const WholeReportPage: React.FC = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <div style={{ position: 'relative' }}>
+        <BackButton />
+        <TraineeHeader title={'Whole Report'} />
+      </div>
+      <main style={{ flex: 1, overflowY: 'auto', padding: '10px' }}>
+        <ReportTable />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default WholeReportPage;
