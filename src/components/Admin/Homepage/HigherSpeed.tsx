563a1089e63ebbd8a8bb3d3a2280a608dedddca2
abhiramsanand <abhiramcr7003@gmail.com>
2024-08-06 10:54:36 +0530

    updates

---
+++ src/components/Admin/Homepage/HigherSpeed.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { Box, Typography, Button } from '@mui/material';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+interface HigherSpeedProps {
+  selectedBatch: number;
+}
+
+const HigherSpeed: React.FC<HigherSpeedProps> = ({ selectedBatch }) => {
+  const [speedPercentage, setSpeedPercentage] = useState<number>(70);
+
+  useEffect(() => {
+    const batchData: Record<number, number> = {
+      1: 70,
+      2: 60,
+      3: 50,
+      4: 40,
+    };
+    setSpeedPercentage(batchData[selectedBatch] || 70);
+  }, [selectedBatch]);
+
+  const data = {
+    labels: ['Viewing videos in higher speed'],
+    datasets: [
+      {
+        label: 'Percentage',
+        data: [speedPercentage, 100 - speedPercentage],
+        backgroundColor: [
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(75, 192, 192, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+    cutout: '70%',
+  };
+
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" mt={4} p={4} borderRadius={2} boxShadow={3}>
+      <Box mt={4} width="100%">
+        <Doughnut data={data} options={options} />
+      </Box>
+      <Box mt={2} position="absolute">
+        <Typography variant="h6">Viewing videos in higher speed</Typography>
+        <Typography variant="h4" fontWeight="bold">{speedPercentage}%</Typography>
+      </Box>
+      <Box mt={2} width="100%" textAlign="right">
+        <Button variant="text" sx={{ color: 'purple' }}>
+          List Trainees
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default HigherSpeed;
--- src/pages/Admin/AdminHomePage.tsx
+++ src/pages/Admin/AdminHomePage.tsx
@@ -1,11 +1,26 @@
-import BatchSelect from "../../components/Admin/Homepage/BatchSelect"
+import { Box } from "@mui/material";
+import BatchSelect from "../../components/Admin/Homepage/BatchSelect";
+import ProgressTracker from "../../components/Admin/Homepage/ProgressTracker";
+import { useState } from "react";
+import HigherSpeed from "../../components/Admin/Homepage/HigherSpeed";
+import DailyReportTrack from "../../components/Admin/Homepage/DailyReportTrack";
 
 const AdminHomePage = () => {
-  return (
-    <div>
-      <BatchSelect/>
-    </div>
-  )
-}
+  const [selectedBatch, setSelectedBatch] = useState<number>(1);
 
-export default AdminHomePage
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <BatchSelect
+        selectedBatch={selectedBatch}
+        onBatchSelect={setSelectedBatch}
+      />
+      <Box display="flex" flexDirection="row" alignItems="center">
+        <ProgressTracker selectedBatch={selectedBatch} />
+        <HigherSpeed selectedBatch={selectedBatch} />
+        <DailyReportTrack selectedBatch={selectedBatch} />
+      </Box>
+    </Box>
+  );
+};
+
+export default AdminHomePage;
