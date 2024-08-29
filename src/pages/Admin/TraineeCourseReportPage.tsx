64996ba93ad7ce08bbf1aedb7c373656477f66c4
athirark96 <athirabala10@gmail.com>
2024-08-29 11:52:05 +0530

    added report table for batch

---
+++ src/pages/Admin/TraineeCourseReportPage.tsx
@@ -0,0 +1,15 @@
+import TraineeCourseReport from "../../components/Admin/TraineeCourseReport/TraineeCourseReport";
+import ReportPage from "./ReportPage";
+import React from 'react'
+
+const TraineeCourseReportPage = () => {
+  return (
+    <div>
+        {/* <ReportPage/> */}
+      <TraineeCourseReport/>
+      
+    </div>
+  )
+}
+
+export default TraineeCourseReportPage
