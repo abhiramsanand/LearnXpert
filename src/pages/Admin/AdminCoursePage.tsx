06ed36aeceaa96e02f0508b256cbb2934bae6365
abhiramsanand <abhiramcr7003@gmail.com>
2024-08-31 19:23:59 +0530

    done

--- src/pages/Admin/AdminCoursePage.tsx
+++ src/pages/Admin/AdminCoursePage.tsx
@@ -19,23 +19,23 @@ const AdminCoursePage = () => {
       alignItems="center"
       position="relative"
     >
-      <Typography
-        align="center"
-        sx={{
-          color: "#8061C3",
-          mb: 2,
-          ml: "-3",
-          fontWeight: "bold",
-          fontSize: "20px",
-        }}
-      >
-        COURSES
-      </Typography>
       <Box mb={2} display="flex" justifyContent="space-between" width="100%">
         <BatchSelect
           selectedBatch={selectedBatch || 0}
           onBatchSelect={handleBatchSelect}
         />
+        <Typography
+          align="center"
+          sx={{
+            color: "#8061C3",
+            mb: 2,
+            ml: "-3",
+            fontWeight: "bold",
+            fontSize: "20px",
+          }}
+        >
+          COURSES
+        </Typography>
         <Link to="/Admin-CourseCreation">
           <Button
             variant="contained"
