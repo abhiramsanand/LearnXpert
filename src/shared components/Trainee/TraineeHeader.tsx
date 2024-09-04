0c8693ba1f0744d089b613541ac680079797063b
Sreeson N <sreeson.n@experionglobal.local>
2024-09-04 15:40:44 +0530

    added username

--- src/shared components/Trainee/TraineeHeader.tsx
+++ src/shared components/Trainee/TraineeHeader.tsx
@@ -1,8 +1,10 @@
-import { AppBar, Container, Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { AppBar, Container, Box, Typography, Avatar } from "@mui/material";
 import { Logout as LogoutIcon } from "@mui/icons-material";
 import { styled } from "@mui/system";
 import NotificationPage from "../../pages/Trainee/NotificationPage";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const HeaderContainer = styled(AppBar)({
   backgroundColor: "rgba(128, 97, 195, 0.1)",
@@ -14,6 +16,24 @@ const HeaderContainer = styled(AppBar)({
 });
 
 const TraineeHeader = () => {
+  const [username, setUsername] = useState("");
+  const traineeId = localStorage.getItem("traineeId");
+
+  useEffect(() => {
+    const fetchUsername = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/v1/profiles/${traineeId}`
+        );
+        setUsername(response.data.userName);
+      } catch (error) {
+        console.error("Error fetching the username:", error);
+      }
+    };
+
+    fetchUsername();
+  }, [traineeId]);
+
   return (
     <>
       <HeaderContainer position="fixed">
@@ -21,10 +41,44 @@ const TraineeHeader = () => {
           <Box
             sx={{ display: "flex", justifyContent: "flex-end", mt: 2, gap: 4 }}
           >
+            <Box sx={{ display: "flex", alignItems: "center", mt: -1, gap: 2 }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                }}
+              >
+                <Typography
+                  variant="body1"
+                  sx={{ color: "rgba(128, 97, 195, 1)" }}
+                >
+                  {username}
+                </Typography>
+                {/* <Typography
+                  variant="caption"
+                  sx={{ textAlign: "center", color: "rgba(128, 97, 195, 0.7)" }}
+                >
+                  (Trainee)
+                </Typography> */}
+              </Box>
+            </Box>
+
             <NotificationPage />
-            <Link to="/" style={{ textDecoration: 'none' }}>
-              <LogoutIcon sx={{ color: "rgba(128, 97, 195, 1)", cursor: 'pointer' }} />
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <LogoutIcon
+                sx={{ color: "rgba(128, 97, 195, 1)", cursor: "pointer" }}
+              />
             </Link>
+            <Avatar
+              sx={{
+                bgcolor: "rgba(128, 97, 195, 0.3)",
+                mt: -1,
+                color: "rgba(128, 97, 195, 1)",
+              }}
+            >
+              {username.charAt(0).toUpperCase()}
+            </Avatar>
           </Box>
         </Container>
       </HeaderContainer>
