ef06a22bf8f36bf2d7daa6f97dbba4a9ecdda1ad
abhiramsanand <abhiramcr7003@gmail.com>
2024-10-22 12:23:44 +0530

    done

--- src/components/Admin/Homepage/DailyReportTrack.tsx
+++ src/components/Admin/Homepage/DailyReportTrack.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useState } from "react";
 import { Doughnut } from "react-chartjs-2";
 import { Box, Typography } from "@mui/material";
